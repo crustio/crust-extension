3d20afe6157c2ac69434313793c45c664579aa84
Deyang Liu <ldy0909061@gmail.com>
2021-03-15 13:00:20 +0800

    Remove test cases

--- test/backgroundScript/services/app-service.test.js
+++
@@ -1,9 +0,0 @@
-import * as appService from '../../../backgroundScript/services/app-service';
-import { mockHashKey } from '../../lib/constants/app-state';
-
-jest.mock('../../../lib/services/extension/storage');
-
-test('Make app Ready', async () => {
-  const result = await appService.appReady(mockHashKey);
-  expect(result).toMatch(mockHashKey);
-});
